356ca0519e8c17c4f8978ac41e96bc988e641c88
enginkaramm-ux <enginkaramm@gmail.com>
2026-07-01 19:29:59 +0300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,21 @@
+namespace SpriteKind {
+    export const karakter = SpriteKind.create()
+}
+let mySprite = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . f f . . . . . . . . . . . . 
+    . f 7 7 f f . . . . . . . . . . 
+    . f 7 f 5 5 f . . . . . . . . . 
+    . . f 5 5 5 5 f . . . . . . . . 
+    . . f 5 5 5 5 5 f f f . . . . . 
+    . . . f 5 5 5 5 5 5 5 f . . . . 
+    . . . . f 5 5 5 5 5 5 5 f . . . 
+    . . . . . f 5 5 5 5 5 5 5 f f . 
+    . . . . . . f f f 5 5 5 5 5 5 f 
+    . . . . . . . . . f 5 5 5 5 5 f 
+    . . . . . . . . . . f f 5 5 f . 
+    . . . . . . . . . . . . f f . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
